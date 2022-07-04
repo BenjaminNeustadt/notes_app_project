0fb701f969fc31f43fc9ab24a6df5507ca0fa54d
benjamin <b.james.neustadt@gmail.com>
2022-07-04 21:24:22 +0200

    TDD MVP NotesModel
    *Created 3 tests, test drove the class
    - Class instance originally empty
    - Class instance can be added to
    - Class instance returns list in an array
    - Class instance can reset the array containing notes (eliminating them)

---
+++ notesModel.js
@@ -0,0 +1,21 @@
+class NotesModel{
+  constructor(){
+    this.notes = [];
+  }
+
+  getNotes(){
+    return this.notes;
+  }
+
+  addNote(postit){
+    return this.notes.push(postit);
+  }
+
+  reset(){
+    return this.notes.length = 0;
+  }
+
+
+}
+
+module.exports = NotesModel;
---
+++ notesModel.test.js
@@ -0,0 +1,23 @@
+const NotesModel = require('./notesModel')
+
+describe('model', () => {
+  it ('getNotes() initially returns empty array', () => {
+    const model = new NotesModel();
+    expect(model.getNotes()).toEqual([]);
+  });
+
+  it ('addNote() adds a string to the array', () => {
+    const model = new NotesModel();
+    model.addNote('Buy milk');
+    model.addNote('Go to the gym');
+    expect(model.getNotes()).toEqual(['Buy milk', 'Go to the gym']);
+  });
+
+  it ('reset() empties the array of inputs', () => {
+    const model = new NotesModel();
+    model.addNote('Buy milk');
+    model.addNote('Go to the gym');
+    model.reset();
+    expect(model.getNotes()).toEqual([]);
+  });
+});

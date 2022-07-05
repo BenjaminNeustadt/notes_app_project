dc981222c8e5ae20bfb5b9d6d793d7da6cd26e6b
benjamin <b.james.neustadt@gmail.com>
2022-07-05 16:29:05 +0200

    Refactored the notesModel.test file
    * refactored using beforeEach(); creating the instance for all test
    instances, outside of each case.

--- notesModel.test.js
+++ notesModel.test.js
@@ -1,20 +1,26 @@
-const NotesModel = require('./notesModel')
+const NotesModel = require('./notesModel');
+//let model;
 
 describe('model', () => {
+
+  beforeEach(() => {
+     model = new NotesModel();
+  });
+
   it ('getNotes() initially returns empty array', () => {
-    const model = new NotesModel();
+    //const model = new NotesModel();
     expect(model.getNotes()).toEqual([]);
   });
 
   it ('addNote() adds a string to the array', () => {
-    const model = new NotesModel();
+    //const model = new NotesModel();
     model.addNote('Buy milk');
     model.addNote('Go to the gym');
     expect(model.getNotes()).toEqual(['Buy milk', 'Go to the gym']);
   });
 
   it ('reset() empties the array of inputs', () => {
-    const model = new NotesModel();
+    //const model = new NotesModel();
     model.addNote('Buy milk');
     model.addNote('Go to the gym');
     model.reset();
